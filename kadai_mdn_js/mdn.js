2c080a5bd99d815aa03e31e940957c87273987d9
Hika6267 <ritoania0602@gmail.com>
2025-03-04 23:14:10 +0900

    Delete kadai_mdn_js directory
    ２

--- kadai_mdn_js/mdn.js
+++
@@ -1,8 +0,0 @@
-const today = new Date();
-const year = getfullYear ();
-const month = today.getMonth();
-const date = today.getDate();
-
-const formattedDate = `${year}年${month}月${date}日`;
-
-console.log(formattedDate);
